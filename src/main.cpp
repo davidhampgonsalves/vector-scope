69727cb18ace67f2a64ce28be4b602037d104423
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2024-12-11 18:27:49 -0400

    switch to VecctorGFX, add animations, fix bugs, etc

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <driver/dac.h>
 #include <math.h>
+#include <VectorGFX.h>
 
-#include "FastDAC.h"
 #include "scene.h"
-#include "data.h"
+
+VectorGFX gfx;
+uint8_t MAX_CORD = 254;
 
 void setup() 
 {
@@ -12,39 +14,50 @@ void setup()
   dac_output_enable(DAC_CHANNEL_2);
 
   Serial.begin(115200);
+  gfx.begin();
+}
+
+int toCords(uint8_t pt) {
+  return static_cast<int>(pt * 4095.0 / MAX_CORD + 0.5);
 }
 
 unsigned long previousMillis = 0;
 int curFrameIndex = 0;
-
+int curSceneIndex = 0;
+int loopCount = 0;
 void loop() {
   unsigned long currentMillis = millis();
-  int curSceneIndex = 0;
-  int loopCount = 0;
   
   scene s = scenes[sceneOverride < 0 ? curSceneIndex : sceneOverride];
-  if (currentMillis - previousMillis >= s.frameInterval) {
-    previousMillis = currentMillis;
+  if (currentMillis - previousMillis < (1000 / s.frameRate)) 
+    return;
 
-    curFrameIndex += 1; 
-    if(curFrameIndex >= s.frameCount) { 
-      curFrameIndex = 0; 
-      loopCount += 1;
-      if(loopCount >= s.loopCount) {
-        curSceneIndex += 1;
-        if(curSceneIndex >= sceneCount) { curSceneIndex = 0; }
-        loopCount = 0;
-      }
+  previousMillis = currentMillis;
+  curFrameIndex += 1; 
+  if(curFrameIndex >= s.frameCount) { 
+    curFrameIndex = 0; 
+    loopCount += 1;
+    if(loopCount >= s.loopCount) {
+      curSceneIndex += 1;
+      if(curSceneIndex >= sceneCount) 
+        curSceneIndex = 0;
+      loopCount = 0;
     }
   }
 
-  DACPrepare(true);
+  bool isMove = true; // first point is always a move
   for(int i=0 ; i < s.framePointCounts[curFrameIndex] ; i += 2) {
-    if(s.frames[curFrameIndex][i] == 500) continue;
-    // dac_output_voltage(DAC_CHANNEL_1, abs(frames[curFrameIndex][i + 1] - 256));
-    // dac_output_voltage(DAC_CHANNEL_2, frames[curFrameIndex][i] );
-    DAC1Write( abs(s.frames[curFrameIndex][i + 1] - 256));
-    DAC2Write( s.frames[curFrameIndex][i] );
+    int xCord = toCords(abs(s.frames[curFrameIndex][i + 1] - MAX_CORD));
+    int yCord = toCords(s.frames[curFrameIndex][i]);
+
+    if(isMove) {
+      gfx.moveto(xCord, yCord);
+      isMove = false;
+    } else if(s.frames[curFrameIndex][i] == 255u && s.frames[curFrameIndex][i+1] == 255u) {
+      isMove = true; // next point is a path start and is move operation
+    } else
+      gfx.lineto(xCord, yCord);
   }
-  DACUnprepare(true);
+
+  gfx.display();
 }
--- src/scene.h
+++ src/scene.h
@@ -1,7 +1,20 @@
+#include "owl.h"
+#include "frog.h"
+#include "penguin.h"
+
 typedef struct { 
-  int frameInterval; 
+  int frameRate; 
   int frameCount;
   int loopCount;
   int* framePointCounts; 
-  unsigned short** frames;
+  uint8_t** frames;
 } scene;
+
+int sceneCount = 3;
+int sceneOverride = -1;
+scene scenes[] {
+  // interval, frame count, loop, counts, frames
+  {12, owl_frameCount, 5, owl_framePointCounts, owl_frames},
+  {22, frog_frameCount, 5, frog_framePointCounts, frog_frames},
+  {16, penguin_frameCount, 5, penguin_framePointCounts, penguin_frames}
+};
